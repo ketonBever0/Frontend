2d612678d4f5825497055dbe48bf36361fabc9cd
ketonBever0 <100591713+ketonBever0@users.noreply.github.com>
2022-11-30 09:55:37 +0100

    asd

--- tavolsag/src/comp/Main.js
+++ tavolsag/src/comp/Main.js
@@ -5,17 +5,31 @@ function Main() {
 
 
     const [distance, setDistance] = useState([]);
+    const [refresh, setRefresh] = useState(false)
+
+
+    const update = () => {
+        setRefresh(prev => !prev)
+    }
+
 
 
 
     useEffect(() => {
         //fetch('https://www.distance24.org/route.json?stops=Sopron|Szeged').catch(err=>console.log(err));
-        fetch('http://192.168.10:8000/distance')
+        fetch('http://localhost:8000/distance')
             .then(res => res.json())
             .then(data => setDistance(data))
             .catch(err => console.log(err));
-    }, []);
+    }, [refresh]);
 
+
+
+
+
+
+
+    // {distance.stops[0].city} {distance.stops[1].city}
 
 
 
@@ -31,7 +45,7 @@ function Main() {
                 <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">
-                        {distance.stops[0].city} és {distance.stops[1].city} távolsága
+                        {distance.stops[0].city} és {distance.stops[1].city}
                     </h2>
                     <p>{distance.distance}</p>
                     <div className="card-actions justify-end">
