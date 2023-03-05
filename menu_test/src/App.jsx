0d2560d635ca3b979066c39028bbf00442c89f8f
ketonBever0 <lacikurucz09@gmail.com>
2023-03-05 22:40:09 +0100

    reszponzív menü mókolás

---
+++ menu_test/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <div className='bg-neutral-50 min-h-screen'>
+
+
+      <NavBar />
+
+      
+    </div>
+  )
+}
+
+export default App
