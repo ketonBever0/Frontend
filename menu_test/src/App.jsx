b5fedb686934b3a772482f88cccdeca3db87c113
ketonBever0 <lacikurucz09@gmail.com>
2023-03-06 13:24:26 +0100

    képtörlés működik, duplikáció hiba megoldva

--- menu_test/src/App.jsx
+++ menu_test/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
+// import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import About from './components/About'
+import Main from './components/Main'
 import NavBar from './components/NavBar'
 
 function App() {
 
+
+
   return (
     <div className='bg-neutral-50 min-h-screen'>
 
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='*' element={<Main />} />
+          <Route path='/' element={<Main />} />
+          <Route path='/about' element={<About />} />
 
-      <NavBar />
+        </Routes>
+      </Router>
 
-      
+
     </div>
   )
 }
--- menu_test/tailwind.config.cjs
+++ menu_test/tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        rotateArrow: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(180deg)' },
+        },
+
+        openDrawer: {
+          '0%': { opactiy: '0' },
+          '100%': { opacity: '1' },
+        }
+
+      },
+
+      animation: {
+        'rotate-arrow': 'rotateArrow',
+
+        'open-drawer': 'openDrawer'
+
+      }
+
+    },
   },
   plugins: [require("daisyui")],
 }
